1120b8a75901c67a25f6ac459b4a8957972fa5e7
济南-类成旭 <1192919554@qq.com>
2020-08-04 12:14:29 +0800

    工具

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mvn clean package -Dmaven.test.skip=true
+echo '项目打包完成'
+docker login --username=还好遗憾念 registry.cn-hangzhou.aliyuncs.com
+echo '登录密码'
+docker build -t registry.cn-hangzhou.aliyuncs.com/test_1192919554/test_my:latest .
+echo '镜像创建完成'
+docker push registry.cn-hangzhou.aliyuncs.com/test_1192919554/test_my:latest
+echo '镜像推送完成'
